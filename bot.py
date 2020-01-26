16a7830a223d4c47e4d043a06b0785aa46084484
PickBas <sayed.kirill@gmail.com>
2020-01-26 20:14:47 +0300

    Added saving ogg files

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import soundfile as sf
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -15,6 +16,11 @@ def audio_handler(message):
     file_info = bot.get_file(message.voice.file_id)
     print(file_info)
     downloaded_file = bot.download_file(file_info.file_path)
+    f = open('file.ogg', 'wb')
+    f.write(downloaded_file)
+    f.close()
+    # data, samplerate = sf.read(downloaded_file)
+    # sf.write('new_file.wav', data, samplerate)
     bot.send_voice(message.chat.id, downloaded_file)
 
 
