2722bfc3959ac50c954832bd76f40f54e0a671f6
PickBas <sayed.kirill@gmail.com>
2020-01-27 10:57:32 +0300

    added converting

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 import config
-import soundfile as sf
+import subprocess
+import os
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -10,18 +11,31 @@ def welcome(message):
     bot.send_message(message.chat.id, "Welcome, {0.username}!\nSend me a voice message!".format(message.from_user))
 
 
+def saving_file(df):
+    f = open('file.ogg', 'wb')
+    f.write(df)
+    f.close()
+
+
+def sanding_file(message):
+    audio_file = open('file.mp3', 'rb')
+    bot.send_audio(message.chat.id, audio_file)
+    audio_file.close()
+
+
+def removing_files():
+    os.remove('./file.ogg')
+    os.remove('./file.mp3')
+
+
 @bot.message_handler(content_types=['voice'])
 def audio_handler(message):
-    print(message)
     file_info = bot.get_file(message.voice.file_id)
-    print(file_info)
     downloaded_file = bot.download_file(file_info.file_path)
-    f = open('file.ogg', 'wb')
-    f.write(downloaded_file)
-    f.close()
-    # data, samplerate = sf.read(downloaded_file)
-    # sf.write('new_file.wav', data, samplerate)
-    bot.send_voice(message.chat.id, downloaded_file)
+    saving_file(downloaded_file)
+    subprocess.call(['ffmpeg', '-i', 'file.ogg', 'file.mp3'])
+    sanding_file(message)
+    removing_files()
 
 
 bot.polling(none_stop=True)
